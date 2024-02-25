8c620654273c81bcc5658e8c236923dc889598f8
starday119 <133083872+starday119@users.noreply.github.com>
2024-02-26 00:00:37 +0900

    진행사항 보고

--- routes/schedule.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const Scheduleregister = require('../Controllers/scheduleregister');
-
-router.get('/view', Scheduleregister.getclubinfo);
-router.post('/', Scheduleregister.createSchedule);
-
-
-module.exports = router;
--- routes/subclubuser.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const subuserlist = require('../Controllers/subclubusers');
-
-const router = express.Router();
-
-router.get('/getMembers/:subclubname', subuserlist.getMembers);
-
-module.exports = router;
